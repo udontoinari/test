43c2841028ec9de7b9d86155f20cf1f4b000bed1
mp21 <umehara@mms.ne.jp>
2022-02-26 19:18:54 +0900

    Initial commit

--- index.js
+++ index.js
@@ -3,53 +3,86 @@
 
 import fs from 'fs';
 import util from 'util';
-import { join } from 'path';
+import { basename, dirname, join, relative } from 'path';
 import minimist from 'minimist';
 import glob from 'glob';
 import glob2base from 'glob2base';
 import nunjucks from 'nunjucks';
+import beautify from 'js-beautify';
+import chokidar from 'chokidar';
 import chalk from 'chalk';
 
 const argv = minimist(process.argv.slice(2), {
   string: ['dir', 'context'],
-  boolean: [],
+  boolean: ['watch'],
   alias: {
-    dir: 'd',
-    context: 'c'
+    d: 'dir',
+    c: 'context',
+    w: 'watch'
   },
-  default: {
-    dir: '.'
-  }
+  default: {}
 });
 
 const pattern = argv._[0];
 const base = glob2base(glob(pattern));
+argv.dir = argv.dir || base;
+
 const context = {
   _data: null,
-  _assign: {
-    env: process.env.NODE_ENV || 'development'
-  },
   get data() {
     if (!this._data) this.data = argv.context;
     return this._data;
   },
   set data(path) {
     try {
-      const data = JSON.parse(fs.readFileSync(path, 'utf8'));
-      this._data = Object.assign(data, this._assign);
+      this._data = JSON.parse(fs.readFileSync(path));
     } catch (error) {
       console.error(chalk.red(error));
-      this._data = this._assign;
+      this._data = {};
     }
+    this._data.env = process.env.NODE_ENV || 'development';
   }
 };
 
 nunjucks.configure(base, { noCache: true });
 const renderSync = util.promisify(nunjucks.render);
+const rc = join(process.cwd(), '.jsbeautifyrc');
+const options = fs.existsSync(rc) ? JSON.parse(fs.readFileSync(rc)) : {};
 
-console.log(context.data);
+const render = async (path) => {
+  const paths = path ? [path] : glob.sync(pattern, {
+    ignore: '**/_*.*'
+  });
+  for (const path of paths) {
+    const input = relative(base, path);
+    const string = await renderSync(input, context.data).catch(error => {
+      console.error(chalk.red(error));
+    });
+    if (!string) continue;
 
+    const output = join(argv.dir, input.replace(/\.\w+$/, '.html'));
+    fs.mkdirSync(dirname(output), { recursive: true });
+    fs.writeFileSync(output, beautify.html(string, options));
+    console.log(chalk.blue(`Rendered: ${output}`));
+  }
+};
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error(chalk.bgRed(reason, promise));
-});
+if (argv.watch) {
+  const options = {
+    ignoreInitial: true
+  };
+  const watcher = chokidar.watch(pattern, options);
+  watcher.on('ready', () => {
+    console.log(chalk.gray('Watching templates. Press Ctrl-C to stop.'));
+  });
+  watcher.on('all', (eventName, path) => {
+    if (eventName == 'unlink') return;
+    render(basename(path).indexOf('_') == 0 ? null : path);
+  });
+  chokidar.watch(argv.context, options).on('all', () => {
+    context.data = argv.context;
+    render();
+  });
+} else {
+  render();
+}
