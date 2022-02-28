f3e3107c3477624373e63be9901704aa8f39226b
mp21 <umehara@mms.ne.jp>
2022-02-28 17:15:53 +0900

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
+import beautify from 'js-beautify';
+import chalk from 'chalk';
+import chokidar from 'chokidar';
 import fs from 'fs';
-import util from 'util';
-import { basename, dirname, join, relative } from 'path';
-import minimist from 'minimist';
 import glob from 'glob';
 import glob2base from 'glob2base';
+import minimist from 'minimist';
 import nunjucks from 'nunjucks';
-import beautify from 'js-beautify';
-import chokidar from 'chokidar';
-import chalk from 'chalk';
+import path from 'path';
+import util from 'util';
 
 const argv = minimist(process.argv.slice(2), {
   string: ['dir', 'context'],
@@ -33,9 +33,9 @@ const context = {
     if (!this._data) this.data = argv.context;
     return this._data;
   },
-  set data(path) {
+  set data(file) {
     try {
-      this._data = JSON.parse(fs.readFileSync(path));
+      this._data = JSON.parse(fs.readFileSync(file));
     } catch (error) {
       console.error(chalk.red(error));
       this._data = {};
@@ -46,22 +46,22 @@ const context = {
 
 nunjucks.configure(base, { noCache: true });
 const renderSync = util.promisify(nunjucks.render);
-const rc = join(process.cwd(), '.jsbeautifyrc');
+const rc = path.join(process.cwd(), '.jsbeautifyrc');
 const options = fs.existsSync(rc) ? JSON.parse(fs.readFileSync(rc)) : {};
 
-const render = async (path) => {
-  const paths = path ? [path] : glob.sync(pattern, {
+const render = async (file) => {
+  const files = file ? [file] : glob.sync(pattern, {
     ignore: '**/_*.*'
   });
-  for (const path of paths) {
-    const input = relative(base, path);
+  for (const file of files) {
+    const input = path.relative(base, file);
     const string = await renderSync(input, context.data).catch(error => {
       console.error(chalk.red(error));
     });
     if (!string) continue;
 
-    const output = join(argv.dir, input.replace(/\.\w+$/, '.html'));
-    fs.mkdirSync(dirname(output), { recursive: true });
+    const output = path.join(argv.dir, input.replace(/\.\w+$/, '.html'));
+    fs.mkdirSync(path.dirname(output), { recursive: true });
     fs.writeFileSync(output, beautify.html(string, options));
     console.log(chalk.blue(`Rendered: ${output}`));
   }
@@ -71,13 +71,9 @@ if (argv.watch) {
   const options = {
     ignoreInitial: true
   };
-  const watcher = chokidar.watch(pattern, options);
-  watcher.on('ready', () => {
-    console.log(chalk.gray('Watching templates. Press Ctrl-C to stop.'));
-  });
-  watcher.on('all', (eventName, path) => {
+  chokidar.watch(pattern, options).on('all', (eventName, file) => {
     if (eventName == 'unlink') return;
-    render(basename(path).indexOf('_') == 0 ? null : path);
+    render(path.basename(file).indexOf('_') == 0 ? null : file);
   });
   chokidar.watch(argv.context, options).on('all', () => {
     context.data = argv.context;
